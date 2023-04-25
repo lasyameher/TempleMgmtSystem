d9a2f0edd21cfacea88b44f395761c576586b859
Nikhileshlingamgunta <124075195+Nikhileshlingamgunta@users.noreply.github.com>
2023-04-24 23:45:28 -0400

    Add files via upload

---
+++ Client/src/components/DevoteeCalendar.jsx
@@ -0,0 +1,75 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    title: "Vacation",
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: "Conference",
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
+];
+
+function App() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    for (let i = 0; i < allEvents.length; i++) {
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
+  
+     
+    }
+  
+    setAllEvents([...allEvents, newEvent]);
+    setNewEvent({ title: "", start: "", end: "" });
+  }
+  
+
+  return (
+    <div>
+      <Navbar />
+      <div className="mb-20">
+        <div>
+        <Calendar className="relative" localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+      </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
